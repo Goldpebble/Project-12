6d66511d4862f7c735a505d4b427d0958892002c
Goldpebble <94041985+Goldpebble@users.noreply.github.com>
2021-12-01 22:32:53 +0800

    Add files via upload

--- Project 12/sketch.js
+++ Project 12/sketch.js
@@ -1,5 +1,8 @@
 var garden,rabbit,apple,orangeL,redL;
 var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+var score;
+score = 0;
+
 
 
 function preload(){
@@ -27,52 +30,62 @@ function setup(){
 
 function draw() {
   background(0);
-  
-  // rabbiT moving on Xaxis with mouse'
-  rabbit.x = World.mouseX;
-  
-  edges = createEdgeSprites();
-  rabbit.collide(edges);
 
-  var select_sprites = Math.round(random(1,3));
+
 
   
-  if (frameCount % 900 != 0) ;{
-    if (select_sprites == 1) {
+  console.log(frameCount);
+  
+ 
+  if (gameState = 1) {
+    // rabbiT moving on Xaxis with mouse'
+    rabbit.x = World.mouseX;
+  
+    edges = createEdgeSprites();
+    rabbit.collide(edges);
+
+    var select_sprites = Math.round(random(1,3));
+
+    if (frameCount % 60 == 0) {
+      if (select_sprites == 1) {
       createApples();
-    } else if (select_sprites == 2) {
+      } else if (select_sprites == 2) {
       createOrange();
-    }else {
-     createRed();
+      } else {
+      createRed();
+      }
     }
-  }
 
-  console.log(frameCount)
+   
+  }
+  
+  
+  
 
   drawSprites();
 }
 
 function createApples() {
-apple = createSprite(random(50, 350),40, 10, 10);
-apple.addImage(appleImg);
-apple.scale=0.07;
-apple.velocityY = 3;
-apple.lifetime = 150;
+  apple = createSprite(random(50, 350),40, 10, 10);
+  apple.addImage(appleImg);
+  apple.scale=0.07;
+  apple.velocityY = 3;
+  apple.lifetime = 150;
   
 }
 
 function createOrange() {
-orangeL = createSprite(random(50, 350),40, 10, 10);
-orangeL.addImage(orangeImg);
-orangeL.scale=0.08;
-orangeL.velocityY = 3;
-orangeL.lifetime = 150;
+  orangeL = createSprite(random(50, 350),40, 10, 10);
+  orangeL.addImage(orangeImg);
+  orangeL.scale=0.08;
+  orangeL.velocityY = 3;
+  orangeL.lifetime = 150;
 }
 
 function createRed() {
-redL = createSprite(random(50, 350),40, 10, 10);
-redL.addImage(redImg);
-redL.scale=0.06;
+  redL = createSprite(random(50, 350),40, 10, 10);
+  redL.addImage(redImg);
+  redL.scale=0.06;
   redL.velocityY = 3;
   redL.lifetime = 150;
 }
